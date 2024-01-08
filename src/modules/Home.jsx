18a66a647fc2229211e4e2d2a887f3823ceaf8d9
gstasaitis <theqerd@gmail.com>
2024-01-08 11:43:29 +0200

    v2

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,6 +1,45 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Loader from './Loader'
 
 const Home = () => {
+
+    const [cards, setCards] = useState()
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    const fetchData = async () => {
+    try {
+    const resp = await fetch('https://robust-safe-crafter.glitch.me/')
+    const data = await resp.json()
+    setCards(data)
+    setIsLoaded(true)
+    } catch (error) {
+    console.error('Error fetching user data:', error.message)
+    setIsLoaded(true)
+    }
+}
+
+    useEffect(() => {
+    fetchData()
+    }, [])
+
+
+    const filterDataByCity = (city, event) => {
+        const cityBtns = document.getElementsByClassName("city-btn")
+
+        for (const x of cityBtns) {
+            x.classList.remove("active")
+        }
+
+        if (city !=="All") {
+            const filter = cards.filter((x) => x.city === city)
+            setCards(filter)
+        }else {
+            fetchData()
+        }
+        event.currentTarget.classList.add("active")
+    }
+
     return (
     <>
     <header>
@@ -14,16 +53,31 @@ const Home = () => {
         <p>Filter:</p>
         <div className="filter-section">
             <div className="cities">
-                <button className="city-btn active">Vilnius</button>
-                <button className="city-btn">Kaunas</button>
-                <button className="city-btn">Klaipėda</button>
-                <button className="city-btn">All</button>
+            <button className="city-btn active" onClick={() => filterDataByCity("Vilnius")}>Vilnius</button>
+            <button className="city-btn" onClick={() => filterDataByCity("Kaunas")}>Kaunas</button>
+            <button className="city-btn" onClick={() => filterDataByCity("Klaipeda")}>Klaipėda</button>
+            <button className="city-btn" onClick={() => filterDataByCity("All")}>All</button>
             </div>
             <Link to="/form" className="add-property">Add Property +</Link>
         </div>
     </main>
     
-    <section className="cards"></section>
+    <section className="cards">
+        {isLoaded && cards.length > 0 ? (
+        cards.map((card, index) => (
+            <div key={index} className="card">
+            <img src={card.image} alt={card.city} />
+            <div className="text">
+                <h3 className="price">€{card.price}</h3>
+                <p className="city">{card.city}</p>
+                <p>{card.description}</p>
+            </div>
+            </div>
+        ))
+        ) : (
+        <Loader/>
+        )}
+    </section>
     </>
     )
 }
